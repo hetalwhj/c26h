6c98c776f4a03293ddb01ce8aa4e3b67cd05b99e
hetal vaishnav <hetal120985@gmail.com>
2021-02-03 23:31:36 +0530

    abcd

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint=Matter.Constraint
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var clog;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+clog= new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,7 +35,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    var options = {
+        bodyA:bird.body,
+        bodyB:clog.body,
+        stiffness:0.05,
+        length:10
+    }
+    var chain=Constraint.create(options)
+    World.add(world,chain)
 }
 
 function draw(){
@@ -61,4 +68,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    clog.display();
 }
