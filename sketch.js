711d792a034c3d0a0ac69e03fc04b3fb29b3677a
hetal <hetal120985@gmail.com>
2021-06-24 21:25:20 +0530

    abcd

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 clog= new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 305, 450, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
